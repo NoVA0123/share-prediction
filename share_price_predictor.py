50904896501ffc06079dff7b9745c780808443f4
Abhijit Rai <abhijit0123rai@gmail.coom>
2023-12-08 20:04:32 +0530

    completed all the function now I have to implement it

--- share_price_predictor.py
+++ share_price_predictor.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklear.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score, mean_absolute_error
+import pickle
 
 
 # saving the data from yahoo finance
@@ -14,10 +15,51 @@ def stock_data_generator(stock_name):
     data.to_csv('/data/' + stock_name + '.csv')
 
 
-def implemetaion(dataset = False, name):
+'''
+Two ways we can achieve importing our data:
+1. Either by importing the data
+2. By downloading the data
+'''
+
+
+def implemetaion(name, dataset=False):
 
     if dataset:
 
         df = pd.read_csv('/data/' + name + 'csv')
 
+    else:
+
+        df = yf.download(name + '.NS')
+
+    # dropping any NaN values and deleting 'Close' column
+    df.dropna(inplace=True)
+    data = pd.DataFrame()
+    data = df.drop('Close', axis=1)
+
+    '''Creating X variable for independent values
+       and y variable for dependent values'''
+    X = data.drop('Adj Close', axis=1).to_numpy()
+    y = data['Adj Close'].to_numpy()
+
+    # Creating a scaler to scale the values of X
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+
+    # splitting the data
+    x_train, x_test, y_train, y_test = train_test_split(X,
+                                                        y,
+                                                        test_size=0.3,
+                                                        random_state=42)
+
+    # making a model and fitting the data
+    rfr = RandomForestRegressor(n_estimators=100,
+                                random_state=3,
+                                max_samples=0.5,
+                                max_features=0.75,
+                                max_depth=15)
+
+    rfr.fit(x_train, y_train)
+
+    return pickle.dump(rfr, open(f'/models/{name}.pkl', 'wb'))
 
